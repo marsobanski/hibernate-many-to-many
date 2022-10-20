dc853914518051da7627e83fefba19a99fa4c1f5
Marcin Sobański <48548225+marsobanski@users.noreply.github.com>
2022-10-20 18:05:48 +0200

    Initial commit

---
+++ src/com/luv2code/hibernate/demo/DeleteCourseAndReviewsDemo.java
@@ -0,0 +1,51 @@
+package com.luv2code.hibernate.demo;
+
+import com.luv2code.hibernate.entity.Course;
+import com.luv2code.hibernate.entity.Instructor;
+import com.luv2code.hibernate.entity.InstructorDetail;
+import com.luv2code.hibernate.entity.Review;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class DeleteCourseAndReviewsDemo {
+
+    public static void main(String[] args) {
+
+        //sql do zapisywania polskich znaków w bazie:
+        //ALTER TABLE student CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci
+        SessionFactory factory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Instructor.class)
+                .addAnnotatedClass(InstructorDetail.class)
+                .addAnnotatedClass(Course.class)
+                .addAnnotatedClass(Review.class)
+                .buildSessionFactory();
+
+        Session session = factory.getCurrentSession();
+
+        try {
+            session.beginTransaction();
+            int id = 11;
+
+            //get course
+            Course course = session.get(Course.class, id);
+            //print course
+            System.out.println("Course: " + course);
+
+            //print course reviews
+            System.out.println(course.getReviews());
+
+            session.delete(course);
+
+            session.getTransaction().commit();
+            System.out.println("Done!");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            session.close();
+            factory.close();
+        }
+    }
+}
